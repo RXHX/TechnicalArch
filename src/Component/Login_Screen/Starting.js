43757db3d5a48a85b8b02ad0e41a2c067df5b201
Rohan kediyal <rohan.kediyal@gmail.com>
2020-12-14 17:12:28 +0530

    Fourth Commit

---
+++ src/Component/Login_Screen/Starting.js
@@ -0,0 +1,15 @@
+import React,{Component, Fragment} from 'react';
+import {View,Text,StyleSheet, Image, ScrollView, TextInput,TouchableOpacity,Dimensions} from 'react-native';
+class Starting_Screen extends Component {
+    state = {  }
+    render() { 
+        return (
+            <Fragment>
+                <View>
+                    
+                </View>
+            </Fragment>
+          );
+    }
+}
+export default Starting_Screen;
