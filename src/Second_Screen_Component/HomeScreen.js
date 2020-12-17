ffddff5ba743fd981209baf609dd36b70604578a
Rohan kediyal <rohan.kediyal@gmail.com>
2020-12-18 03:02:50 +0530

    Eighth Commit

--- src/Second_Screen_Component/HomeScreen.js
+++ src/Second_Screen_Component/HomeScreen.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import RenderIf from '../Renderer/RenderIf';
 import {
    View,
    Image,StyleSheet,
     Linking,
-  Dimensions,
-  TouchableHighlight
+  Text,
+  TouchableHighlight,
+  Animated,
+  Dimensions
 } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 class ScanScreen extends Component {
@@ -16,9 +18,14 @@ class ScanScreen extends Component {
 constructor(props)
 {
    super(props);
+   this.value = new Animated.ValueXY({
+    x:0,
+    y:Dimensions.get('window').height
+  })
    this.state = {
-     active : false
-   }
+     active : false,
+   };
+   
 
 }
 
@@ -34,16 +41,56 @@ onSuccess = e => {
  
     onclickerable = () => {
       this.setState({
-        active : true
+        active : true,
+        animationactive:this.state.animationactive
       })
      
       console.log('Reached Here'+this.state.active);
    }
  
- 
+   starting = () => {
+    Animated.timing(this.value,{
+      toValue: { x:0, y:30},
+      duration:500,
+      useNativeDriver: false
+    }).start();
+   }
+
+   closing = () => {
+    Animated.timing(this.value,{
+      toValue: { x:0, y:550},
+      duration:500,
+      useNativeDriver: false
+    }).start();
+    
+   }
+
     return (
-      <View style={{ flex:1,backgroundColor: '#fff' }}>
-     <TouchableHighlight onPress = {onclickerable} style = {{height:50,width:50,marginLeft:300}}>
+      <View style = {{flex:1,backgroundColor:'#fff'}}>   
+   
+     <View style = {{flex:1,flexDirection:"row",backgroundColor:"#fff"}}>    
+     
+     
+     <View style = {{height:30,width:30,marginLeft:10,marginTop:10}}>
+     <TouchableHighlight onPress = {starting} activeOpacity={0.6}
+  underlayColor="#FFFFFF">
+     <Image 
+     source = {require('../../assest/Main_Screen/Location.png')}
+     style = {{height:30,width:30}}>
+     </Image>
+     </TouchableHighlight>
+     </View>
+
+
+
+     <View style = {{height:30,width:120,marginLeft:10,marginTop:10}}>
+     <Text style = {styles.textBold}>Other-Patparganj</Text>  
+     </View>
+      
+     
+     <View style = {{height:30,width:30,marginLeft:150,marginTop:10}}>
+     <TouchableHighlight activeOpacity={0.6}
+  underlayColor="#DDDDDD" onPress = {onclickerable} style = {{height:30,width:30}}>
        {
          this.state.active ?
          <QRCodeScanner
@@ -54,38 +101,62 @@ onSuccess = e => {
            :
            <Image 
            source = {require('../../assest/Main_Screen/home.png')}
-           style = {{height:50,width:50}}
+           style = {{height:30,width:30}}
            >
           </Image>
        }
    </TouchableHighlight>
    </View>
-    
-         
-          
-                  
-        );
+
+
+   </View>
+
+ <Animated.View style = {this.value.getLayout()} >
+    <View style={styles.container}>
+      <View style = {{flexDirection:'row'}}>
+      <Text style = {styles.buttonText}>Search Location</Text>
+      <TouchableHighlight onPress = {closing} activeOpacity={0}
+  underlayColor="#FFFFFF">
+      <Image source = {require('../../assest/Main_Screen/cross.png')} style = {{height:60,width:60,marginTop:10,marginLeft:150}} onMagicTap = {closing}></Image>
+      </TouchableHighlight>
+      </View>
+      <View style = {{marginTop:10}}>
+        <TextInput 
+        placeholder = "Search for your location"
+        //  inlineImageLeft = "search"
+        //   inlineImagePadding = {10}
+        style = {{borderRadius:4,borderWidth:3,marginLeft:10,marginRight:10}}
+        />
+        
+      </View>
+      </View> 
+ </Animated.View>
+ </View>
+);
   }
 }
 
 const styles = StyleSheet.create({
-  centerText: {
-    width:40,
-    height:40,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
-  },
   textBold: {
-    fontWeight: '500',
-    color: '#000'
+    fontSize: 15,
+    fontWeight:'bold',
+    color: "#000000",
+    textDecorationLine: 'underline'
   },
   buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
+    fontSize: 15,
+    fontWeight:'bold',
+    color: "#000000",
+    marginTop:10,
+    padding:20
   },
   buttonTouchable: {
-    padding: 16
+    padding: 16,
+    backgroundColor:"#ffff57"
+  },
+  container: {
+    height: 550,
+    width: Dimensions.get('window').width
   }
 });
 export default ScanScreen; 
